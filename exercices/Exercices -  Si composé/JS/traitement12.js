19262d949d89ef969ad9cc45f5ccba8deca9858b
Joseph-elie <josephelieb@gmail.com>
2018-11-02 14:12:06 -0400

    joseph-élie Brassard

--- exercices/Exercices -  Si composé/JS/traitement12.js
+++ exercices/Exercices -  Si composé/JS/traitement12.js
@@ -1,5 +1,26 @@
 function btnCalculer_onclick()
 { 
- 	
+ 	var nbre1,nbre2,nbre3,nbre4;
+
+    nbre1  = parseFloat((document.getElementById("txtNbre1").value));
+    nbre2  = parseFloat((document.getElementById("txtNbre2").value));
+    nbre3  = parseFloat((document.getElementById("txtNbre3").value));
+    nbre4 = 3;
+
+
+    if((nbre1 >= nbre2) && (nbre2 !== nbre3))
+    {
+        nbre4 = nbre4 +2 * nbre1;
+    }
+    else if((nbre1 = nbre2) || (nbre2 > nbre3))
+    {
+        nbre4 = nbre4 + nbre1;
+    }
+    else
+    {
+        nbre4 = nbre4 * nbre2 / nbre1 - nbre3;
+    }
+    document.getElementById("lblMessage").innerHTML = "" + nbre4;
+
 
 }
