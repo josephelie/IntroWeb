1ec42860ec4e059ad96c9171f495d8826434fd0e
Joseph-elie <josephelieb@gmail.com>
2018-11-08 11:08:48 -0500

    joseph-élie Brassard

--- exercices/Exercice _ Joueurs/JS/traitement.js
+++ exercices/Exercice _ Joueurs/JS/traitement.js
@@ -1,4 +1,19 @@
 function btnCalculer_onclick()
 { 
-    var
+    var nom,point;
+
+    nom =(document.getElementById("txtNom").value);
+    point = parse.Float(document.getElementById("txtPoints").value);
+
+    Tabnom[0] = Bob;
+    Tabnom[1] = Richard;
+    Tabnom[2] = Billy;
+    tabnom[3] = Joe;
+    for(i = 0; i< Tabnom.lenght; i++)
+    {
+     if (Tabnom[i] === nom)
+     {
+        alert("nom")
+     }
+    }
 }
