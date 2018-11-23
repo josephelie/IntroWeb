8ff09ba03c56fe8c4e608d5f45971588625c2a57
Joseph-elie <josephelieb@gmail.com>
2018-11-23 09:45:48 -0500

    joseph-élie Brassard

--- exercices/Exercices expressions régulières- Etu/JS/traitement.js
+++ exercices/Exercices expressions régulières- Etu/JS/traitement.js
@@ -14,20 +14,20 @@ function validerTel( Chaine )  // au format (418) 876-8373  un numéro ça chang
 }
 function validerCar( Chaine ) // des chaînes de caractères alphabétiques majuscule et minuscule et accepte le trait d'union pas obligatoire
 {
-   /^[A-Z]|-?[a-z]|$/.test(Chaine);
+   return /^[A-Z]|-?[a-z]|$/.test(Chaine);
 }
 
 function validerNo( Chaine )   // un chiffre peut etre 0 5 25 200 etc..
 {
-
+    /^[0-9]+<0>+$|<5>+$/.test(Chaine);
 }
 
 function verifierEntreDixTrente( Chaine ) //un chiffre entre 10 et 30
 {
-
+   /^[10-30]{1}$/.test(Chaine);
 }
 
 function verifierEntreUnVingt( Chaine ) //un chiffre entre 1 et 20
 {
-
+   /^[1-20]{1}$/.test(Chaine);
 }
